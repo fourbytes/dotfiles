c5672b1ffb4982318d938ee155c6526c30c4cf1d
Oscar Rainford <oscar@fourbs.com.au>
2021-08-15 02:45:57 +1000

    update

--- bin/test-linker.fish
+++ bin/test-linker.fish
@@ -1,36 +1,48 @@
 #!/usr/local/bin/fish
 
-set CRATE $argv[1]
-set LINKER $argv[2]
+set LINKER $argv[1]
 
-set RUSTC_ARGS "-Ztime-passes"
+set -x RUSTFLAGS "-Ztime-passes"
 
 if test -n "$LINKER"
-	set RUSTC_ARGS $RUSTC_ARGS "-Clink-arg=-fuse-ld=$LINKER"
+	set -x RUSTFLAGS $RUSTFLAGS "-Clink-arg=-fuse-ld=$LINKER"
 end
-
-set_color -o; set_color cyan; echo -n "# "; echo -ne (echo "testing linker" | string pad -w15)
-set_color normal; echo " $LINKER"
-set_color -o; set_color cyan; echo -n "# "; echo -ne (echo "rustc flags" | string pad -w15)
-set_color normal; echo " $RUSTC_ARGS"
+set_color -o; set_color cyan; echo -n "# "; echo -ne (echo "crate: " | string pad -w16)
+set_color normal; echo (pwd | string split '/')[-1]
+set_color -o; set_color cyan; echo -n "# "; echo -ne (echo "rustc version: " | string pad -w16)
+set_color normal; echo (rustc -V)
+set_color -o; set_color cyan; echo -n "# "; echo -ne (echo "rustc target: " | string pad -w16)
+set_color normal; echo (rustc -vV | grep host | string split -n ' ')[2]
+set_color -o; set_color cyan; echo -n "# "; echo -ne (echo "testing linker: " | string pad -w16)
+set_color normal; echo "$LINKER"
+set_color -o; set_color cyan; echo -n "# "; echo -ne (echo "rustc flags: " | string pad -w16)
+set_color normal; echo "$RUSTFLAGS"
 echo
-echo -n "> "
-for i in (seq 0 15)
-	touch src/main.rs
-	set result $result (cargo rustc --bin $CRATE -- \
-		$RUSTC_ARGS \
-		2>| rg run_linker \
-		| string split -n \t \
-		| string split -n ' ')[2]
-	
-	echo -n "$result[-1] "
+for i in (seq 0 10)
+	# touch src/main.rs
+	set total 0
+	set_color -d; echo -n "pass $i => "; set_color normal
+	cargo clean
+	cargo build 2>&1 | rg run_linker | while read -L line
+		set time (echo $line \
+			| string split -n \t \
+			| string split -n ' ')[2]
+		set total (math $total + $time)
+	end
+	echo -n "$total secs total"
+	if test $i -eq 0
+		echo " (skipped)"
+	else 
+		set -g result $result $total
+		echo
+	end
 end
-echo
-echo
 set total 0
 for i in $result
 	set total (math $total + $i)
 end
 set average (math $total / (count $result))
-set_color -o; set_color red; echo -n "# "; echo -ne (echo "average" | string pad -w15)
-set_color normal; echo " $average secs"
+set_color -o; set_color red; echo -ne (echo "average => " | string pad -w16)
+set_color normal; echo "$average secs"
+echo
+echo "---"
